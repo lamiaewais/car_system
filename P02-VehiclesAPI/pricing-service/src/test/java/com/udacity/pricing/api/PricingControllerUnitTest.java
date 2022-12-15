9b2f2a395c1be5c180a73eaef7c0521831a5bbef
lamiaewais <lamia5980@gmail.com>
2022-12-15 13:50:01 +0300

    clean code

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerUnitTest.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerUnitTest.java
@@ -10,13 +10,9 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.math.BigDecimal;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
