2369bb499339da558822c71ad1826c84131564eb
lamiaewais <lamia5980@gmail.com>
2022-12-15 13:46:15 +0300

    add unit and integration test

---
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerUnitTest.java
@@ -0,0 +1,50 @@
+package com.udacity.pricing.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private MockedStatic<PricingService> mockedStatic;
+
+    @Before
+    public void setUp() {
+        mockedStatic = Mockito.mockStatic(PricingService.class);
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+        Price price = new Price("USD", new BigDecimal(500), 1L);
+        mockedStatic.when(() -> PricingService.getPrice(1L)).thenReturn(price);
+        String  expectedResult = objectMapper.writeValueAsString(price);
+
+        mockMvc.perform(get("/services/price?vehicleId=1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedResult));
+    }
+}
